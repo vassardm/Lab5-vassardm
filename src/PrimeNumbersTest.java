64a8960ba0893d6083e1a3fcde4df7e34a37217c
Demetruis Vassar <vassardm@rose-hulman.edu>
2012-03-25 15:16:30 -0400

    Passed big Number of 325115

--- src/PrimeNumbersTest.java
+++ src/PrimeNumbersTest.java
@@ -48,6 +48,11 @@ public class PrimeNumbersTest {
 		assertEquals(list (3, 3), PrimeNumbers.generatePrimes(9));
 	}
 	
+	@Test
+	public void testBigNumber() {
+		assertEquals(list (5, 7, 7, 1327), PrimeNumbers.generatePrimes(325115));
+	}
+	
 	private ArrayList<Integer> list(int...ints) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for (int i: ints) {
