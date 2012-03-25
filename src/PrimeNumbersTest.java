421688ea5f1fbeb45e0132b24fffdc46bac20d85
Demetruis Vassar <vassardm@rose-hulman.edu>
2012-03-25 14:42:08 -0400

    testJUnit is working

---
+++ src/PrimeNumbersTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PrimeNumbersTest {
+	@Test
+	public void testJUnit() {
+		assertTrue("The compiler isn't feeling well today...", true);
+	}
+}
