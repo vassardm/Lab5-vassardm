3a0468b1eda4ac20941256cadd0e53363970412a
Demetruis Vassar <vassardm@rose-hulman.edu>
2012-03-25 15:00:16 -0400

    Adding TestFour

--- src/PrimeNumbersTest.java
+++ src/PrimeNumbersTest.java
@@ -28,6 +28,11 @@ public class PrimeNumbersTest {
 		assertEquals(list(3), PrimeNumbers.generatePrimes(3));
 	}
 	
+	@Test
+	public void testFour() {
+		assertEquals(list (2, 2), PrimeNumbers.generatePrimes(4));
+	}
+	
 	private ArrayList<Integer> list(int...ints) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for (int i: ints) {
